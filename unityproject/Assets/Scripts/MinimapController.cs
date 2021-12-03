15edd0e1e6900eebfab627ef0468eb587bd1bed6
sterenziani <sterenziani@itba.edu.ar>
2021-12-03 05:12:11 -0300

    Made minimap follow players

--- unityproject/Assets/Scripts/MinimapController.cs
+++ unityproject/Assets/Scripts/MinimapController.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class MinimapController : MonoBehaviour
 {
 	public Transform target1;
+	public Transform icon1;
 	public Transform target2;
-	
+	public Transform icon2;
+	private float distance = 50;
+
     void Update()
     {
         if(target1 != null)
@@ -14,13 +17,21 @@ public class MinimapController : MonoBehaviour
 			if (target2 != null)
 			{
 				float x = (target1.position.x + target2.position.x) / 2;
-				float y = 20 + (target1.position.y + target2.position.y) / 2;
+				float y = distance + (target1.position.y + target2.position.y) / 2;
 				float z = (target1.position.z + target2.position.z) / 2;
-				transform.position = new Vector3(x, transform.position.y, z);
+				float size = Mathf.Sqrt(Mathf.Pow(target1.position.x + target2.position.x, 2) + Mathf.Pow(target1.position.z + target2.position.z, 2));
+				float scale = size / distance;
+
+				transform.position = new Vector3(x, y, z);
+				GetComponent<Camera>().orthographicSize = size;
+				if (icon1 != null)
+					icon1.localScale = new Vector3(scale, scale, scale);
+				if (icon2 != null)
+					icon2.localScale = new Vector3(scale, scale, scale);
 			}
 			else
 			{
-				transform.position = new Vector3(target1.position.x, 20 + target1.position.y, target1.position.z);
+				transform.position = new Vector3(target1.position.x, distance + target1.position.y, target1.position.z);
 			}
 		}
     }
